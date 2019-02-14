06fe385a1388ece33217f3b82e43040d3b53893c
Madesaad <45132888+Madesaad@users.noreply.github.com>
2019-02-14 13:06:49 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@
 const client = new Discord.Client();
 const client2 = new Discord.Client();
 const myid = ['536186199987978240'];
-const prefix = ['!'];
+const prefix = ['*'];
 
 client.on('ready', () => {
    console.log(`----------------`);
